e0fe70dadc209f6888b67932791479160832e6d3
Sam Kim <fxf24@naver.com>
2017-03-16 15:26:27 +0900

    FirstLayout finish

--- app/src/main/java/com/example/qudqj_000/myapplication/MainActivity.java
+++ app/src/main/java/com/example/qudqj_000/myapplication/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.qudqj_000.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText e1, e2, e3;
+    Button b1, b2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("학점 계산기");
+        program();
+    }
+
+    void program() {
+
     }
 }
